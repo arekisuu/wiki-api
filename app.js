9571181a4dbee64bbd1543d242abbaa6ec37b6be
Alex <95551477+arekisuu@users.noreply.github.com>
2022-03-18 18:25:16 -0300

    Add missing requests & minified ver.

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const app = express();
 // Set EJS as the view engine for the project
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-// Set the 'public' folder as the location for static files
-app.use(express.static("public"));
 
 // Connect Node server to local MongoDB database
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
@@ -21,58 +19,118 @@ const articleSchema = {
   content: String
 };
 
-// Create a model based on the appropriate schema
+// Create a model based on the above schema
 const Article = mongoose.model("Article", articleSchema);
 
-// Handle 'get' requests on the /articles route
-app.get("/articles", (req, res) => {
-  // Find every item in the collection
-  Article.find((err, results) => {
-    if (!err) {
-      // Send the found items back if there are no errors
-      res.send(results);
-    } else {
-      // Send back the error(s) if there's any
-      res.send(err);
-    }
+// Requests targetting all articles
+app.route("/articles")
+  .get((req, res) => {
+    Article.find((err, results) => {
+      if (!err) {
+        // Send the found items back if there are no errors
+        res.send(results);
+      } else {
+        // Send back the error(s) if there's any
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    // Create a new article based on the 'Article' model
+    const newArticle = new Article({
+      // Use data provided by the post request as the values for the new article
+      title: req.body.title,
+      content: req.body.content
+    });
+    // Attempt to save the new article and run the if condition
+    newArticle.save((err) => {
+      // Save the new article if there are no errors
+      if (!err) {
+        // Send back a success message if all goes well
+        res.send("Successfully added a new article.");
+      } else {
+        // Send back the error(s) if there's any
+        res.send(err);
+      }
+    });
+  })
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      // Delete all articles if there are no errors
+      if (!err) {
+        // Send back a success message if all goes well
+        res.send("Successfully deleted all articles.");
+      } else {
+        // Send back the error(s) if there's any
+        res.send(err);
+      }
+    });
   });
-});
 
-// Handle 'post' requests on the /articles route
-app.post("/articles", (req, res) => {
-  // Create a new article based on the 'Article' model
-  const newArticle = new Article({
-    // Use the sent data as the values for the new article
-    title: req.body.title,
-    content: req.body.content
+// Requests targetting a specific article
+app.route("/articles/:article")
+  .get((req, res) => {
+    Article.findOne(
+      // Query for the specified article
+      {title: req.params.article},
+      (err, result) => {
+        if (result) {
+          // Send back the article if it is found
+          res.send(result);
+        } else {
+          // Send back an error message if none is found
+          res.send("No articles match specified title.");
+        }
+      });
+  })
+  .put((req, res) => {
+    Article.replaceOne(
+      // Query for the specified article
+      {title: req.params.article},
+      // Overwrite ALL the article data using the values provided by the request (deletes fields if no new value is given)
+      {title: req.body.title,content: req.body.content},
+      (err) => {
+        if (!err) {
+          // Send back a success message if all goes well
+          res.send("Successfully overwritten article.");
+        } else {
+          // Send back the error(s) if there's any
+          res.send(err);
+        }
+      });
+  })
+  .patch((req, res) => {
+    Article.updateOne(
+      // Query for the specified article
+      {title: req.params.article},
+      // The $set operator keeps all non-specified fields unchanged and only updates what's provided by the request
+      {$set: req.body},
+      (err) => {
+        if (!err) {
+          // Send back a success message if all goes well
+          res.send("Successfully updated article.");
+        } else {
+          // Send back the error(s) if there's any
+          res.send(err);
+        }
+      }
+    );
+  })
+  .delete((req, res) => {
+    Article.deleteOne(
+      // Query for the specified article
+      {title: req.params.article},
+      (err) => {
+        if (!err) {
+          // Send back a success message if all goes well
+          res.send("Successfully deleted article.");
+        } else {
+          // Send back the error(s) if there's any
+          res.send(err);
+        }
+      }
+    );
   });
-  // Attempt to save the new article and run the if condition
-  newArticle.save((err) => {
-    // Save the new article if there are no errors
-    if (!err) {
-      // Send back a success message in case all goes well
-      res.send("Successfully added a new article.");
-    } else {
-      // Send back the error(s) if there's any
-      res.send(err);
-    }
-  });
-});
-
-// Handle 'delete' requests on the /articles route
-app.delete("/articles", (req, res) => {
-  // Attempt to delete all articles and run the if condition
-  Article.deleteMany((err) => {
-    // Delete all articles if there are no errors
-    if (!err) {
-      // Send back a success message in case all goes well
-      res.send("Successfully deleted all articles.");
-    } else {
-      // Send back the error(s) if there's any
-      res.send(err);
-    }
-  });
-});
 
 // Listen on specified port and logs on success
 app.listen(process.env.PORT || 3000, () => {console.log("Server running on port 3000")});
